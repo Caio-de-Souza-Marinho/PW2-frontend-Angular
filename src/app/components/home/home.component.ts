b4d92b3cd06af4a75ae08c9dbf9556f5ec97658b
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-03 20:43:39 -0300

    [Aula 03/05/22] Adicionando variáveis

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  nomeProduto: string = 'Curso Angular';
+
+  //template string 👇 strings definidas com crase permitem a interpolação, permitindo colocar variáveis dentro da string
+  anuncio: string = `O ${this.nomeProduto} está em promoção!`;
+
+  idProduto: number = 123;
+  precoProduto: number = 2.59;
+  promocao: boolean = true;
+
+
+  constructor() {
+
+    console.log('Nome do produto: ', this.nomeProduto);
+    console.log('Anuncio: ', this.anuncio);
+    console.log('ID: ', this.idProduto);
+    console.log('Preço: ', this.precoProduto);
+    console.log('Promoção: ', this.promocao);
+
+   }
 
   ngOnInit(): void {
   }
