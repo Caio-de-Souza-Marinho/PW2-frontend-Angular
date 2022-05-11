5a18135e521ce78db04bad54c2fc98c868a90447
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-10 22:01:54 -0300

    [Aula 10/05/22] Adiconando formatação de variáveis utilizando pipes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,35 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   nomeProduto: string = 'Curso Angular';
   //template string 👇 strings definidas com crase permitem a interpolação, permitindo colocar variáveis dentro da string
   anuncio: string = `O ${this.nomeProduto} está em promoção!`;
-  idProduto: number = 123;
-  precoProduto: number = 2.59;
+  idProduto: number = 3;
+  precoProduto: number = 2.51;
   promocao: boolean = false;
-  foto: string ="assets/img/crud.png"
-
+  foto: string = 'assets/img/crud.png';
+  dataValidade = "2021-12-31"
 
   constructor() {
-
     // console.log('Nome do produto: ', this.nomeProduto);
     // console.log('Anuncio: ', this.anuncio);
     // console.log('ID: ', this.idProduto);
     // console.log('Preço: ', this.precoProduto);
     // console.log('Promoção: ', this.promocao);
-
-
-
-
-
-
-}
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {}
 }
