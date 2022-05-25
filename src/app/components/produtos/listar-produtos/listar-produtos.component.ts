387718d1508470f8e229710ff0278886d474710f
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-05-24 22:41:08 -0300

    [Aula 24/05/22] Adicionando lógica do componente listar produtos

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-produtos',
   templateUrl: './listar-produtos.component.html',
-  styleUrls: ['./listar-produtos.component.css']
+  styleUrls: ['./listar-produtos.component.css'],
 })
 export class ListarProdutosComponent implements OnInit {
+  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumeros: number[] = [15, 15.18, 100];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    for(let item of this.listaStrings){
+      console.log(item);
+    }
+    for(const item of this.listaNumeros){
+      console.log(item);
+    }
   }
 
+  ngOnInit(): void {}
 }
