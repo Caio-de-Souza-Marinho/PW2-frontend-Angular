a14b4994ac7ccaf8c8375a9319acf99614a2d466
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-06-05 02:41:40 -0300

    Adicionando objeto lista de protudos

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -9,6 +9,13 @@ export class ListarProdutosComponent implements OnInit {
   listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
   listaNumeros: number[] = [15, 15.18, 100];
 
+  objetoModelo = {
+    nome: 'Caio',
+    idade: 21,
+    altura: 1.60,
+    graduado: true
+  };
+
   constructor() {
     for(let item of this.listaStrings){
       console.log(item);
@@ -16,6 +23,9 @@ export class ListarProdutosComponent implements OnInit {
     for(const item of this.listaNumeros){
       console.log(item);
     }
+
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
   }
 
   ngOnInit(): void {}
