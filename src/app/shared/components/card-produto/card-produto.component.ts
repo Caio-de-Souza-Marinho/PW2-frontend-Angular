879c712f5c6e8df6a14784c4251d0eb56e46b070
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-06-07 20:09:42 -0300

    Inserindo variáveis no card-produto component

--- src/app/shared/components/card-produto/card-produto.component.ts
+++ src/app/shared/components/card-produto/card-produto.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-produto',
   templateUrl: './card-produto.component.html',
-  styleUrls: ['./card-produto.component.css']
+  styleUrls: ['./card-produto.component.css'],
 })
 export class CardProdutoComponent implements OnInit {
+  @Input() foto: string = '';
+  @Input() nomeProduto: string = '';
+  @Input() promocao: boolean = true;
+  @Input() idProduto: number = 0;
+  @Input() dataValidade: string = '';
+  @Input() precoProduto: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
