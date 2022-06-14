dadd1260c9b9260f8ec14fa6429b247e49943784
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-06-14 19:50:18 -0300

    Adicionando método para exibir mensagem no toastr

--- src/app/services/produtos.service.ts
+++ src/app/services/produtos.service.ts
@@ -2,6 +2,7 @@ import { IProduto } from './../model/IProduto.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +10,23 @@ import { Observable } from 'rxjs';
 export class ProdutosService {
   private URL: string = 'http://localhost:3000/produtos';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private toastr: ToastrService) {}
 
   buscarTodos(): Observable<IProduto[]>{
     return this.http.get<IProduto[]>(this.URL);
   }
+
+  exibirMensagem(titulo: string, mensagem: string, tipo: string): void {
+    this.toastr.show(mensagem, titulo, {closeButton:true, progressBar: true}, tipo);
+  }
+
+
+
+
+
+
+
+
+
+
 }
