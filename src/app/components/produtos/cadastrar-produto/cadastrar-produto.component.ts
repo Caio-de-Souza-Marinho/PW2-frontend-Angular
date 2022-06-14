9249fb1ed380efa6e212822636a22dd1f590959d
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-06-14 20:37:19 -0300

    Adicionando método para salvar produto

--- src/app/components/produtos/cadastrar-produto/cadastrar-produto.component.ts
+++ src/app/components/produtos/cadastrar-produto/cadastrar-produto.component.ts
@@ -1,5 +1,7 @@
+import { ProdutosService } from './../../../services/produtos.service';
 import { IProduto } from './../../../model/IProduto.model';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-produto',
@@ -7,16 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar-produto.component.css'],
 })
 export class CadastrarProdutoComponent implements OnInit {
-
   produto: IProduto = {
     nome: '',
     validade: new Date(),
-    precoProduto: 0
+    precoProduto: 0,
   };
 
-  constructor() {}
+  constructor(private produtosService: ProdutosService, private router: Router) {}
+
+  salvarProduto(): void {
+    this.produtosService.cadastrar(this.produto).subscribe(retorno => {
+      this.produto = retorno;
+      this.produtosService.exibirMensagem(
+        'Sistema',
+        `${this.produto.nome} foi cadastrado com sucesso. ID: ${this.produto.id}`,
+        'toast-success'
+      );
+      this.router.navigate(['/produtos']);
+    });
+  }
 
   ngOnInit(): void {}
-
-
 }
